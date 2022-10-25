6258c7e5b80d8beb66bac09356dadff593005688
DarkMoonLi <45242101+DarkMoonLi@users.noreply.github.com>
2022-10-25 21:30:28 +0300

    Update structure.

---
+++ FarmaNetFrontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { InputComponent } from './pages/input/input.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    RegistrationComponent,
+    InputComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
