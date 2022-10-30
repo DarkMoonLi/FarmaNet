954d9497c94164a36d7e143c5c0aad4e3f6be5b6
Artem Pershutkin <79787099+Artom-art@users.noreply.github.com>
2022-10-30 19:10:49 +0300

    Add files via upload

--- FarmaNet.sql
+++ FarmaNet.sql
@@ -2,94 +2,95 @@ USE FarmaNet;
 
 CREATE TABLE Pharmacy
 (
-	Id INT PRIMARY KEY IDENTITY,
+	IdPharmacy INT PRIMARY KEY IDENTITY,
 	Name VARCHAR(50) NOT NULL,
 	Address VARCHAR(255) NOT NULL,
-	Email VARCHAR(50) NOT NULL,
+	Email VARCHAR(100) NULL,
 	Description VARCHAR(1000) NULL
 );
 
 CREATE TABLE Position
 (
-	Id INT PRIMARY KEY IDENTITY,
+	IdPosition INT PRIMARY KEY IDENTITY,
 	Position VARCHAR(30) NOT NULL,
 	SalaryInHours DECIMAL(5, 2) NOT NULL
 );
 
 CREATE TABLE WorkerInformation
 (
-	Id INT PRIMARY KEY IDENTITY,
-	Name VARCHAR(20) NOT NULL,
-	LastName VARCHAR(30) NOT NULL,
+	IdWorkerInformation INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(50) NOT NULL,
+	LastName VARCHAR(50) NOT NULL,
 	DataBirth DATE NOT NULL,
 	SeriesPassport TINYINT NOT NULL,
 	NumberPassport TINYINT NOT NULL,
 	Experience TINYINT NULL,
-	Email VARCHAR(50) NOT NULL,
+	Email VARCHAR(100) NULL,
 	IdPosition INT NOT NULL,
-	FOREIGN KEY (IdPosition) REFERENCES Position (Id),
+	FOREIGN KEY (IdPosition) REFERENCES Position (IdPosition),
 );
 
 CREATE TABLE WorkerAccount
 (
-	Id INT PRIMARY KEY IDENTITY,
+	IdWorkerAccount INT PRIMARY KEY IDENTITY,
 	Login VARCHAR(255) NOT NULL,
 	Password VARCHAR(255) NOT NULL,
 	IdWorkerInformation INT NOT NULL,
 	IdPharmacy INT NOT NULL,
 	FOREIGN KEY (IdPharmacy) REFERENCES Pharmacy (Id),
-	FOREIGN KEY (IdWorkerInformation) REFERENCES WorkerInformation (Id)
+	FOREIGN KEY (IdWorkerInformation) REFERENCES WorkerInformation (IdWorkerInformation)
 );
 
+/*Нужно создать таблицу для связи типа медикамента с медикаментом*/
 CREATE TABLE TypeMedication
 (
-	Id INT PRIMARY KEY IDENTITY,
+	IdTypeMedication INT PRIMARY KEY IDENTITY,
 	Name VARCHAR(80) NOT NULL,
 );
 
 CREATE TABLE Medication
 (
-	Id INT PRIMARY KEY IDENTITY,
-	Name VARCHAR(20) NOT NULL,
-	Recipe VARCHAR(1) NOT NULL,
-	IdType INT NOT NULL,
-	FOREIGN KEY (IdType) REFERENCES TypeMedication (Id)
+	IdMedication INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(50) NOT NULL,
+	Recipe VARCHAR(500) NOT NULL,
+	IdTypeMedication INT NOT NULL,
+	FOREIGN KEY (IdTypeMedication) REFERENCES TypeMedication (IdTypeMedication)
 );
 
 CREATE TABLE Supplier
 (
-	Id INT PRIMARY KEY IDENTITY,
-	Name VARCHAR(30) NOT NULL,
+	IdSupplier INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(50) NOT NULL,
 	Inn VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Import
 (
-	Id INT PRIMARY KEY IDENTITY,
+	IdImport INT PRIMARY KEY IDENTITY,
 	Number SMALLINT NOT NULL,
 	Date DATE NOT NULL,
 	SumPrice DECIMAL(11, 2) NOT NULL,
 	IdSupplier INT NOT NULL,
 	IdPharmacy INT NOT NULL,
-	FOREIGN KEY (IdSupplier) REFERENCES Supplier (Id),
-	FOREIGN KEY (IdPharmacy) REFERENCES Pharmacy (Id)
+	FOREIGN KEY (IdSupplier) REFERENCES Supplier (IdSupplier),
+	FOREIGN KEY (IdPharmacy) REFERENCES Pharmacy (IdPharmacy)
 );
 
 CREATE TABLE Manufacturer
 (
-	Id INT PRIMARY KEY IDENTITY,
-	Name VARCHAR(30) NOT NULL,
+	IdManufacturer INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(100) NOT NULL,
 	Address VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE WorkingHours
 (
-	Id INT PRIMARY KEY IDENTITY,
-	IdWorker INT NOT NULL,
+	IdWorkingHours INT PRIMARY KEY IDENTITY,
+	IdWorkerAccount INT NOT NULL,
 	Date DATE NOT NULL,
 	Time TIME NOT NULL,
-	Description VARCHAR(255) NOT NULL,
-	FOREIGN KEY (IdWorker) REFERENCES WorkerAccount (Id)
+	Description VARCHAR(1000) NOT NULL,
+	FOREIGN KEY (IdWorkerAccount) REFERENCES WorkerAccount (IdWorkerAccount)
 );
 
 CREATE TABLE Sale
@@ -97,8 +98,8 @@ CREATE TABLE Sale
 	IdMedication INT NOT NULL,
 	IdPharmacy INT NOT nULL,
 	PRIMARY KEY (IdMedication, IdPharmacy),
-	FOREIGN KEY (IdMedication) REFERENCES Medication (Id),
-	FOREIGN KEY (IdPharmacy) REFERENCES Pharmacy (Id)
+	FOREIGN KEY (IdMedication) REFERENCES Medication (IdMedication),
+	FOREIGN KEY (IdPharmacy) REFERENCES Pharmacy (IdPharmacy)
 );
 
 CREATE TABLE WriteDowns
@@ -107,8 +108,8 @@ CREATE TABLE WriteDowns
 	IdPharmacy INT NOT NULL,
 	Quantity INT NOT NULL,
 	PRIMARY KEY (IdMedication, IdPharmacy),
-	FOREIGN KEY (IdMedication) REFERENCES Medication (Id),
-	FOREIGN KEY (IdPharmacy) REFERENCES Pharmacy (Id)
+	FOREIGN KEY (IdMedication) REFERENCES Medication (IdMedication),
+	FOREIGN KEY (IdPharmacy) REFERENCES Pharmacy (IdPharmacy)
 );
 
 CREATE TABLE ImportWithMedication
@@ -118,8 +119,8 @@ CREATE TABLE ImportWithMedication
 	Quantity INT NOT NULL,
 	Price DECIMAL(7, 2) NOT NULL,
 	PRIMARY KEY (IdInvoice, IdMedication),
-	FOREIGN KEY (IdInvoice) REFERENCES Import (Id),
-	FOREIGN KEY (IdMedication) REFERENCES Medication (Id)
+	FOREIGN KEY (IdInvoice) REFERENCES Import (IdImport),
+	FOREIGN KEY (IdMedication) REFERENCES Medication (IdMedication)
 );
 
 CREATE TABLE MedicationWithManufacturer
@@ -127,8 +128,8 @@ CREATE TABLE MedicationWithManufacturer
 	IdManufacturer INT NOT NULL,
 	IdMedication INT NOT NULL,
 	PRIMARY KEY (IdManufacturer, IdMedication),
-	FOREIGN KEY (IdManufacturer) REFERENCES Manufacturer (Id),
-	FOREIGN KEY (IdMedication) REFERENCES Medication (Id)
+	FOREIGN KEY (IdManufacturer) REFERENCES Manufacturer (IdManufacturer),
+	FOREIGN KEY (IdMedication) REFERENCES Medication (IdMedication)
 );
 
 CREATE TABLE PharmacyWithMedication
@@ -138,6 +139,6 @@ CREATE TABLE PharmacyWithMedication
 	Quantity INT NOT NULL,
 	Price DECIMAL(7, 2) NOT NULL,
 	PRIMARY KEY (IdMedication, IdPharmacy),
-	FOREIGN KEY (IdMedication) REFERENCES Medication (Id),
-	FOREIGN KEY (IdPharmacy) REFERENCES Pharmacy (Id)
+	FOREIGN KEY (IdMedication) REFERENCES Medication (IdMedication),
+	FOREIGN KEY (IdPharmacy) REFERENCES Pharmacy (IdPharmacy)
 );
