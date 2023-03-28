0c63b345496c109d425b59a964b1889b5e65ea2a
irina.koskina <irina.koskina@travelline.ru>
2023-03-28 09:13:54 +0300

    feat: Таблица накладных

--- FarmaNetFrontend/src/app/pages/imports/imports.component.ts
+++ FarmaNetFrontend/src/app/pages/imports/imports.component.ts
@@ -1,81 +1,5 @@
-// import { Component, OnInit } from '@angular/core';
-// import { NgModule } from '@angular/core';
-// import { ImportDto } from 'src/app/dto/import.dto';
-
-// @Component({
-//   selector: 'app-imports',
-//   templateUrl: './imports.component.html',
-//   styleUrls: ['./imports.component.scss']
-// })
-
-// export class ImportsComponent implements OnInit {
-//   data: ImportDto[] = [
-//     {ImportId: 0, Number: 1, Date: '01.01.2023', SumPrice: 12000, SupplierId: 4, PharmacyId: 1 },
-//     {ImportId: 0, Number: 2, Date: '01.01.2023', SumPrice: 12000, SupplierId: 5, PharmacyId: 2 },
-//     {ImportId: 0, Number: 3, Date: '01.01.2023', SumPrice: 12000, SupplierId: 6, PharmacyId: 1 }
-//   ]
-//   public dataSource: ImportDto[] = this.data;
-
-//   displayedColumns: string[] = ['ImportId', 'Number', 'Date', 'SumPrice', 'SupplierId', 'PharmacyId'];
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-// }
-
-// // @NgModule({
-// //   imports: [MatTableModule]
-// // })
-// // export class AppModule { }
-
-// // export class TableColumnStylingExample {
-// //   displayedColumns: string[] = ['ImportId', 'Number', 'Date', 'SumPrice', 'SupplierId', 'PharmacyId'];
-// //   dataSource = this.dataSource;
-// // }
-
-// // const data: ImportDto[] = [
-// //   {ImportId: 0, Number: 1, Date: '01.01.2023', SumPrice: 12000, SupplierId: 4, PharmacyId: 1 },
-// //   {ImportId: 0, Number: 2, Date: '01.01.2023', SumPrice: 12000, SupplierId: 5, PharmacyId: 2 },
-// //   {ImportId: 0, Number: 3, Date: '01.01.2023', SumPrice: 12000, SupplierId: 6, PharmacyId: 1 }
-// // ]
-
-
-// import { Component, OnInit } from '@angular/core';
-// import { ImportDto } from 'src/app/dto/import.dto';
-
-// const ELEMENT_DATA: ImportDto[] = [
-//   {ImportId: 0, Number: 1, Date: '01.01.2023', SumPrice: 12000, SupplierId: 4, PharmacyId: 1 },
-//   {ImportId: 34, Number: 2, Date: '01.01.2023', SumPrice: 12000, SupplierId: 5, PharmacyId: 2 },
-//   {ImportId: 90, Number: 3, Date: '01.01.2023', SumPrice: 12000, SupplierId: 6, PharmacyId: 1 }
-// ];
-
-// /**
-//  * @title Basic use of `<table mat-table>`
-//  */
-// @Component({
-//   selector: 'app-imports',
-//   styleUrls: ['./imports.component.scss'],
-//   templateUrl: './imports.component.html',
-// })
-// export class ImportsComponent implements OnInit {
-//   public displayedColumns: string[] = ['ImportId', 'Number', 'Date', 'SumPrice', 'SupplierId', 'PharmacyId'];
-//   public dataSource = ELEMENT_DATA;
-  
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-// }
-
-// @Component({
-//   selector: 'app-imports',
-//   templateUrl: './imports.component.html',
-//   styleUrls: ['./imports.component.scss']
-// })
-
-
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ImportDto } from '../../dto/import.dto';
 
 export interface PeriodicElement {
   name: string;
@@ -84,32 +8,60 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: ImportDto[] = [
+  {ImportId: 0, Number: "1", Date: '01.01.2023', SumPrice: 12000, SupplierId: 4, PharmacyId: 1 },
+  {ImportId: 34, Number: "2", Date: '01.01.2023', SumPrice: 12000, SupplierId: 5, PharmacyId: 2 },
+  {ImportId: 90, Number: "3", Date: '01.01.2023', SumPrice: 12000, SupplierId: 6, PharmacyId: 1 }
 ];
-
-/**
- * @title Basic use of `<table mat-table>`
- */
-// selector: 'app-imports',
-//   styleUrls: ['./imports.component.scss'],
-//   templateUrl: './imports.component.html',
 
 @Component({
   selector: 'app-imports',
   styleUrls: ['./imports.component.scss'],
   templateUrl: './imports.component.html',
 })
-export class ImportsComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+export class ImportsComponent implements OnInit {
+  product = {} as ImportDto;
+  // product: ImportDto | undefined;   // изменяемый товар
+  products: ImportDto[] | undefined = ELEMENT_DATA;                // массив товаров
+  tableMode: boolean = true;          // табличный режим
+
+  constructor() { }
+
+  ngOnInit() {
+      // this.loadProducts();    // загрузка данных при старте компонента  
+  }
+  // получаем данные через сервис
+  loadProducts() {
+      // this.dataService.getProducts()
+      //     .subscribe((data: Product[]) => this.products = data);
+  }
+  // сохранение данных
+  save() {
+      // if (this.product.id == null) {
+      //     this.dataService.createProduct(this.product)
+      //         .subscribe((data: Product) => this.products.push(data));
+      // } else {
+      //     this.dataService.updateProduct(this.product)
+      //         .subscribe(data => this.loadProducts());
+      // }
+      this.cancel();
+  }
+  editProduct(p: ImportDto) {
+      this.product = p;
+  }
+  cancel() {
+      this.product = {} as ImportDto;
+      this.tableMode = true;
+  }
+  delete(p: ImportDto) {
+      // this.dataService.deleteProduct(p.id)
+      //     .subscribe(data => this.loadProducts());
+  }
+  add() {
+      this.cancel();
+      this.tableMode = false;
+  }
+  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  // dataSource = ELEMENT_DATA;
+
 }
